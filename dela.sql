e403281945af68a5ca52422ef2cd4bf520e2e6d3
Wolverator <andrey131295@yandex.ru>
2017-03-18 01:29:23 +0300

    Added DB

---
+++ dela.sql
@@ -0,0 +1,38 @@
+-- phpMyAdmin SQL Dump
+-- version 3.5.1
+-- http://www.phpmyadmin.net
+--
+-- Хост: 127.0.0.1
+-- Время создания: Мар 18 2017 г., 02:28
+-- Версия сервера: 5.5.25
+-- Версия PHP: 5.3.13
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- База данных: `dela`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `todolist`
+--
+
+CREATE TABLE IF NOT EXISTS `todolist` (
+  `added` datetime NOT NULL,
+  `text` text,
+  `dobefore` datetime DEFAULT NULL,
+  PRIMARY KEY (`added`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
